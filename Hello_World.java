e66aa31d88c9c1e90a5572f397838f77ad77b88f
angelmercy2310-beep <angelmercy2310@gmail.com>
2025-10-25 15:35:28 +0530

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,18 @@
+// HelloWorldModerate.java
+// A simple Java program to print Hello World with a little extra info
+
+public class HelloWorldModerate {
+    public static void main(String[] args) {
+        // Print main greeting
+        System.out.println("Hello, World!");
+
+        // Print an introduction message
+        System.out.println("This is my first Java program.");
+
+        // Print a small tip
+        System.out.println("Java is fun and easy to learn!");
+
+        // Print closing message
+        System.out.println("Keep practicing and exploring Java!");
+    }
+}
